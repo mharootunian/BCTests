bd1b32d1a2806154249696ec0e57ee2b240c37c8
Meero Harootunian <meero.harootunian@gmail.com>
2019-10-05 12:42:47 -0700

    2

--- logic.js
+++ logic.js
@@ -2,6 +2,9 @@ $.ajax ({
     method: "GET",
     url: "https://api.twitter.com/oauth2/token",
     data: "grant_type=client_credentials",
+    user: "EFRoCsMwQ4ZpqZF2oUzidrGg1:x2E0E3qH3rcSc4ZuMF52IVDCTqNwOw4BN8Uqj4B6V4byIgqCSC"
 }).then(function(response) {
     console.log(response)
+}, function(errResponse) { 
+    console.log(`Error: ${errResponse}`)
 })
